93b962c6297f29ad850a6f2feacc6dbc407785d9
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2018-11-15 00:52:24 +0100

    ---------- Social Educ ----------
    Student profile:
    
    - finish hidden form ("Ajouter des experience")

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-profile.component.css']
 })
 export class StudentProfileComponent implements OnInit {
-
-  constructor() { }
+  checkbox: any;
+  constructor() {}
 
   ngOnInit() {
+    this.openForm();
+    this.closeForm();
+    this.actualRole();
   }
+
+openForm(): void {
+    document.getElementById("myExperienceForm").style.display = "block";
+}
+
+closeForm(): void {
+    document.getElementById("myExperienceForm").style.display = "none";
+}
+
+actualRole(): void {
+  this.checkbox = document.getElementById("working-now-checkbox");
+
+  if(this.checkbox.checked == false){
+    document.getElementById("months-to-id").style.display = "block";
+    document.getElementById("show-present-txt").style.display = "none"; 
+  }else{
+    document.getElementById("months-to-id").style.display = "none";
+    document.getElementById("show-present-txt").style.display = "block";
+  }
+  
+}
 
 }
