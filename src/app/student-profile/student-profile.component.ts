556b2df97f6773db116f6cf7ed4e607c90fc0de5
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2018-11-15 19:12:27 +0100

    ---------- Social Educ ----------
    Student profile:
    
    - finish hidden form ("Ajouter des écoles")
    
    - add additional:
      - if the user leaves any hidden form, the values of those field are rested.

--- src/app/student-profile/student-profile.component.ts
+++ src/app/student-profile/student-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-student-profile',
@@ -6,27 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-profile.component.css']
 })
 export class StudentProfileComponent implements OnInit {
-  checkbox: any;
+  private checkbox: any;
+  private ok: boolean;
+  private i: number;
+
   constructor() {}
 
   ngOnInit() {
-    this.openForm();
-    this.closeForm();
+    this.openExperienceForm();
+    this.closeExperienceForm();
+
+    this.openEducationForm();
+    this.closeEducationForm();
+
     this.actualRole();
   }
-
-openForm(): void {
-    document.getElementById("myExperienceForm").style.display = "block";
+  
+openExperienceForm(): void {
+  document.getElementById("myExperienceForm").style.display = "block";
 }
 
-closeForm(): void {
-    document.getElementById("myExperienceForm").style.display = "none";
+closeExperienceForm(): void {
+  document.getElementById("myExperienceForm").style.display = "none";
+}
+
+openEducationForm(): void {
+  document.getElementById("myEducationForm").style.display = "block";
+}
+
+closeEducationForm(): void {
+  document.getElementById("myEducationForm").style.display = "none";
 }
 
 actualRole(): void {
   this.checkbox = document.getElementById("working-now-checkbox");
 
-  if(this.checkbox.checked == false){
+  if(this.checkbox.checked == true){
     document.getElementById("months-to-id").style.display = "block";
     document.getElementById("show-present-txt").style.display = "none"; 
   }else{
